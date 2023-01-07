a1b093dc09689fe6575b02d1cbc5d0f0aaa6c3d2
kxkx5150 <senna5150ando@gmail.com>
2023-01-08 01:16:56 +0900

    remove xhost

--- mikanos/sh/run.sh
+++ mikanos/sh/run.sh
@@ -18,21 +18,15 @@ if [ -d apps ]; then
     done
 fi
 
-# xhost +local: 
-# Error "gtk initialization failed" 
 if [ "${APPFLAG}" = "app" ] ; then
-    xhost +
     sleep 1
     MIKANOS_DIR=$PWD $HOME/osbook/devenv/run_mikanos.sh
     echo run_mikanos
     sleep 1
-    xhost -
 else
-    xhost +
     sleep 1
     $HOME/osbook/devenv/run_qemu.sh $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi kernel/kernel.elf
     echo run_qemu
     sleep 1
-    xhost -
 fi
 
